3db41e090a8095fdf9d169b248983d54aa311edf
jackSpanrrows <jack.spanrrows@gmail.com>
2019-10-05 15:05:45 +0800

    update v1.0.2

--- init.sh
+++ init.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-echo "现在拉取分支代码"
-git fetch -p
+echo 开始执行时间 `date +"%Y-%m-%d %H:%M:%S"`
+echo "现在拉取分支更新"
+git fetch -p > run.log
+echo `date +"%Y-%m-%d %H:%M:%S"`
+cat run.log
 echo "拉取代码"
-git pull
+git pull > run.log
+echo `date +"%Y-%m-%d %H:%M:%S"`
+cat run.log
 
-echo "更新模块版"
+echo "更新模块版本"
 go mod tidy
-go mod download
-
+go mod download	
+echo All update complete `date +"%Y-%m-%d %H:%M:%S"`
